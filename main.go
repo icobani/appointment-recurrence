ed760df061fa40af18c69957d26fd447f3817658
salvobabani92 <salvatorbabani@gmail.com>
2019-04-03 17:41:58 +0300

    Gerekli girişler yapıldı.

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package appointment_recurrence
 
-/*
-func SetExecutionTimes() {
-	log.Println("SetExecution Times")
-	var broadcastJobTasks []BroadcastJobTask
+func main() {
 
-	this.DB.Find(&broadcastJobTasks,
-		"active = ? AND "+
-			"(next_executing_time = ? OR next_executing_time is null) AND "+
-			"(end_date >= ? OR no_end_date = ?)",
-		true,
-		"0001-01-01",
-		time.Now().Format("2006-01-02 15:04:00"),
-		true)
-
-	for _, job := range broadcastJobTasks {
-		job.Calc()
-		this.DB.Save(&job)
-
-	}
 }
-
-
